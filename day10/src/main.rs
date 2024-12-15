5b6fbbfd1903bc5095d77db81981ec49b84594f4
TcePrepK <a.s.arda.simsek@gmail.com>
2024-12-16 02:11:27 +0300

    Day 10 🧹

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -5,15 +5,42 @@ fn read_file(path: &str) -> String {
     fs::read_to_string(path).expect("Could not read file")
 }
 
-#[allow(unused_variables)]
-fn main() {
-    let example = read_file("src/example");
-    let input = read_file("src/input");
-    let used_string = input;
+const ANSWER_ONE: i32 = 36;
+const ANSWER_TWO: i32 = 81;
 
-    println!("First Part: {:?}", first_part(&used_string));
-    println!("Second Part: {:?}", second_part(&used_string));
+fn test_examples() -> [bool; 2] {
+    let example = read_file("src/example");
+
+    let results = [first_part(&example), second_part(&example)];
+
+    if results[0] > 0 && results[0] != ANSWER_ONE {
+        println!("Part One Wrong");
+    }
+
+    if results[1] > 0 && results[1] != ANSWER_TWO {
+        println!("Part Two Wrong");
+    }
+
+    [results[0] == ANSWER_ONE, results[1] == ANSWER_TWO]
 }
+
+fn test_inputs(example_solutions: [bool; 2]) {
+    let input = read_file("src/input");
+
+    if example_solutions[0] {
+        println!("Part One: {:?}", first_part(&input));
+    }
+    if example_solutions[1] {
+        println!("Part Two: {:?}", second_part(&input));
+    }
+}
+
+fn main() {
+    let example_solutions = test_examples();
+    test_inputs(example_solutions);
+}
+
+/* ------------------- Helpers ------------------- */
 
 fn parse_input(input: &str) -> Vec<Vec<usize>> {
     input
@@ -51,6 +78,8 @@ fn find_neighbors(grid: &Vec<Vec<usize>>, x: usize, y: usize) -> Vec<(usize, usi
 
     result
 }
+
+/* ------------------- Solutions ------------------- */
 
 #[allow(unused_variables)]
 fn first_part(input: &str) -> i32 {
