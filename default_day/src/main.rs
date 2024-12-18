5bb759abf0bb9cbd3fb282205ee4860c53caa482
TcePrepK <a.s.arda.simsek@gmail.com>
2024-12-19 00:03:40 +0300

    src code to copy

---
+++ default_day/src/main.rs
@@ -0,0 +1,73 @@
+use std::fs;
+
+fn read_file(path: &str) -> String {
+    if fs::exists(path).expect("Could not check file") {
+        fs::read_to_string(path).expect("Could not read file")
+    } else {
+        String::new()
+    }
+}
+
+const ANSWER_ONE: i32 = -1;
+const ANSWER_TWO: i32 = -1;
+
+fn test_examples() -> (bool, bool) {
+    let mut example_1 = read_file("src/example_1");
+    let mut example_2 = read_file("src/example_2");
+    if example_1.is_empty() && !example_2.is_empty() {
+        panic!("Example 1 is empty, but example 2 is not");
+    } else if !example_1.is_empty() && example_2.is_empty() {
+        panic!("Example 2 is empty, but example 1 is not");
+    } else if example_1.is_empty() && example_2.is_empty() {
+        example_1 = read_file("src/example");
+        example_2 = example_1.clone();
+    }
+
+    let results = (first_part(&example_1), second_part(&example_2));
+
+    if results.0 != 0 && results.0 != ANSWER_ONE {
+        println!("Part One Wrong");
+    }
+
+    if results.1 != 0 && results.1 != ANSWER_ONE {
+        println!("Part Two Wrong");
+    }
+
+    (results.0 == ANSWER_ONE, results.1 == ANSWER_TWO)
+}
+
+fn test_inputs(example_solutions: (bool, bool)) {
+    let input = read_file("src/input");
+
+    if example_solutions.0 {
+        let start_time = std::time::Instant::now();
+        let result = first_part(&input);
+        let total_time = start_time.elapsed();
+        println!("Part 1 result: {}, took: {:?}", result, total_time);
+    }
+    if example_solutions.1 {
+        let start_time = std::time::Instant::now();
+        let result = second_part(&input);
+        let total_time = start_time.elapsed();
+        println!("Part 2 result: {}, took: {:?}", result, total_time);
+    }
+}
+
+fn main() {
+    let example_solutions = test_examples();
+    test_inputs(example_solutions);
+}
+
+/* ------------------- Helpers ------------------- */
+
+/* ------------------- Solutions ------------------- */
+
+#[allow(unused_variables)]
+fn first_part(input: &str) -> i32 {
+    0
+}
+
+#[allow(unused_variables)]
+fn second_part(input: &str) -> i32 {
+    0
+}
