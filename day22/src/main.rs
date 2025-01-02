33a3991761aa5efe0f2a84ff14010c7cae337496
TcePrepK <a.s.arda.simsek@gmail.com>
2025-01-02 04:06:18 +0300

    Day 22 🚀

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,3 +1,4 @@
+use rayon::prelude::*;
 use utils::test_solutions;
 
 fn main() {
@@ -9,120 +10,95 @@ fn main() {
 /// Parses and turns the given input into a vector of numbers
 fn get_numbers(input: &str) -> Vec<u32> {
     input
-        .split("\n")
+        .lines()
         .map(|s| s.parse::<u32>().unwrap())
         .collect::<Vec<u32>>()
 }
 
-/// Calculates the sequence of numbers while putting the difference sequence of 4 into the cache...
-/// Annoying day
-fn cache_sequence(
-    mut num: u32,
-    input_idx: usize,
-    input_len: usize,
-    n: usize,
-    prev_four: &mut [i32; 4],
-    bool_cache: &mut [bool; 130320],
-    number_cache: &mut [u32; 130320],
-    highest_score: &mut u32,
-) {
-    let prev = num % 10;
+fn get_next(num: u32) -> u32 {
+    let mut num = num;
     num ^= num << 6;
     num &= 0xffffff;
     num ^= num >> 5;
     num ^= num << 11;
     num &= 0xffffff;
-    let secret = num % 10;
+    num
+}
 
-    prev_four[3] = prev_four[2];
-    prev_four[2] = prev_four[1];
-    prev_four[1] = prev_four[0];
-    prev_four[0] = secret as i32 - prev as i32;
+/// Calculates the sequence of numbers while putting the difference sequence of 4 into the cache...
+/// Annoying day
+fn cache_sequence(
+    bool_cache: &mut [bool; 130321],
+    number_cache: &mut [u32; 130321],
+    num: u32,
+    highest_number: &mut u32,
+) {
+    let num_0 = num; // 123456
+    let num_1 = get_next(num_0); // 15887950
+    let num_2 = get_next(num_1); // 16495136
+    let mut prev = get_next(num_2); // 527345
 
-    if 2000 - n >= 4 {
-        // Use base19 to base10 conversation to get the hash id (18, 18, 18, 18) -> 130320
-        let hash_id: usize = (prev_four[0] + 9) as usize * 19 * 19 * 19
-            + (prev_four[1] + 9) as usize * 19 * 19
-            + (prev_four[2] + 9) as usize * 19
-            + (prev_four[3] + 9) as usize;
+    let mut diff_0 = 9 + num_1 % 10 - num_0 % 10; // -3 + 9
+    let mut diff_1 = 9 + num_2 % 10 - num_1 % 10; // 6 + 9
+    let mut diff_2 = 9 + prev % 10 - num_2 % 10; // -1 + 9
+
+    for _ in 3..2000 {
+        let next = get_next(prev);
+        let secret = next % 10;
+        let diff_3 = 9 + secret - prev % 10;
+        prev = next;
+
+        let hash_id: usize = diff_0 as usize * 19 * 19 * 19
+            + diff_1 as usize * 19 * 19
+            + diff_2 as usize * 19
+            + diff_3 as usize;
+
+        diff_0 = diff_1;
+        diff_1 = diff_2;
+        diff_2 = diff_3;
 
         // If the hash id is not already in the current input cache, add it
-        if !bool_cache[hash_id] {
-            bool_cache[hash_id] = true;
+        if bool_cache[hash_id] {
+            continue;
+        }
+        bool_cache[hash_id] = true;
+        number_cache[hash_id] += secret;
 
-            if secret != 0 {
-                let cached_value = &mut number_cache[hash_id];
-                if *cached_value < u32::MAX {
-                    *cached_value += secret;
-
-                    if highest_score < cached_value {
-                        *highest_score = *cached_value;
-                    }
-                } else {
-                    number_cache[hash_id] = secret;
-                }
-            }
+        if number_cache[hash_id] > *highest_number {
+            *highest_number = number_cache[hash_id];
         }
     }
-
-    if n == 1 {
-        return;
-    }
-
-    cache_sequence(
-        num,
-        input_idx,
-        input_len,
-        n - 1,
-        prev_four,
-        bool_cache,
-        number_cache,
-        highest_score,
-    );
 }
 
 /* ------------------- Solutions ------------------- */
 
-#[allow(unused_variables)]
 fn first_part(input: &str) -> u64 {
     let numbers = get_numbers(input);
-    let mut last_score = 0;
-
-    for number in numbers {
-        let mut num = number;
-        for i in 0..2000 {
-            num ^= num << 6;
-            num &= 0xffffff;
-            num ^= num >> 5;
-            num ^= num << 11;
-            num &= 0xffffff;
-        }
-        last_score += num as u64;
-    }
-
-    last_score
+    numbers
+        .par_iter()
+        .map(|&number| {
+            let mut num = number;
+            for _ in 0..2000 {
+                num ^= num << 6;
+                num &= 0xffffff;
+                num ^= num >> 5;
+                num ^= num << 11;
+                num &= 0xffffff;
+            }
+            num as u64
+        })
+        .sum::<u64>()
 }
 
-#[allow(unused_variables)]
 fn second_part(input: &str) -> u32 {
     let numbers = get_numbers(input);
-    let highest_score: &mut u32 = &mut 0;
 
-    let input_len = numbers.len();
-    let mut number_cache: [u32; 130320] = [u32::MAX; 130320];
-    for (input_idx, number) in numbers.iter().enumerate() {
-        let mut bool_cache: [bool; 130320] = [false; 130320];
-        cache_sequence(
-            *number,
-            input_idx,
-            input_len,
-            2000,
-            &mut [0; 4],
-            &mut bool_cache,
-            &mut number_cache,
-            highest_score,
-        );
-    }
+    let highest_number = &mut 0;
+    let mut number_cache = [0; 130321];
+    numbers.iter().for_each(|&number| {
+        let mut bool_cache = [false; 130321];
+        cache_sequence(&mut bool_cache, &mut number_cache, number, highest_number);
+    });
 
-    *highest_score
+    *number_cache.iter().max().unwrap()
 }
